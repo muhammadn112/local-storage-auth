b3e47ec53dcd74ed9b835f7b94ccf44510e800f2
muhammadnoorani112 <mamu.noorani@gmail.com>
2024-03-21 11:03:48 +0530

    release 1.0

---
+++ script.js
@@ -0,0 +1,36 @@
+function openLoginPopup() {
+    document.getElementById('loginPopup').style.display = 'block';
+  }
+  
+  function openSignupPopup() {
+    document.getElementById('signupPopup').style.display = 'block';
+  }
+  
+  function closePopup() {
+    document.getElementById('loginPopup').style.display = 'none';
+    document.getElementById('signupPopup').style.display = 'none';
+  }
+  
+  function signUp() {
+    var username = document.getElementById('signupUsername').value;
+    var password = document.getElementById('signupPassword').value;
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
+    alert('Sign Up Successfull');
+    closePopup();
+  }
+  
+  function login() {
+    var username = document.getElementById('loginUsername').value;
+    var password = document.getElementById('loginPassword').value;
+    var storedUsername = localStorage.getItem('username');
+    var storedPassword = localStorage.getItem('password');
+  
+    if (username === storedUsername && password === storedPassword) {
+      alert('Login successful!');
+      closePopup();
+    } else {
+      document.getElementById('loginErrorMessage').innerText = 'Username or password is incorrect.';
+    }
+  }
+  
