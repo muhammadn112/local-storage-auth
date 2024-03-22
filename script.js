f5d71d651c8c57fd83a5a22f7f6c8a1c73d3bb0a
muhammadnoorani112 <mamu.noorani@gmail.com>
2024-03-22 13:22:43 +0530

    release 1.1

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
+
 function openLoginPopup() {
-    document.getElementById('loginPopup').style.display = 'block';
-  }
-  
-  function openSignupPopup() {
-    document.getElementById('signupPopup').style.display = 'block';
-  }
-  
-  function closePopup() {
-    document.getElementById('loginPopup').style.display = 'none';
-    document.getElementById('signupPopup').style.display = 'none';
-  }
-  
-  function signUp() {
-    var username = document.getElementById('signupUsername').value;
-    var password = document.getElementById('signupPassword').value;
-    localStorage.setItem('username', username);
-    localStorage.setItem('password', password);
-    alert('Sign Up Successfull');
-    closePopup();
-  }
-  
-  function login() {
-    var username = document.getElementById('loginUsername').value;
-    var password = document.getElementById('loginPassword').value;
-    var storedUsername = localStorage.getItem('username');
-    var storedPassword = localStorage.getItem('password');
-  
-    if (username === storedUsername && password === storedPassword) {
-      alert('Login successful!!!!!');
-      closePopup();
-    } else {
-      document.getElementById('loginErrorMessage').innerText = 'Username or password is incorrect.';
-    }
-  }
-  
+  $('#loginPopup').fadeIn();
+}
+
+function openSignupPopup() {
+  $('#signupPopup').fadeIn();
+}
+
+
+function closePopup() {
+  $('.popup').fadeOut();
+}
+
+$(document).ready(function () {
+
+  $('#loginForm').parsley();
+  $('#signupForm').parsley();
+
+
+  $('#signupForm').on('submit', function (e) {
+      e.preventDefault();
+      if ($(this).parsley().isValid()) {
+          var username = $('#signupUsername').val().trim();
+          var password = $('#signupPassword').val().trim();
+          localStorage.setItem('username', username);
+          localStorage.setItem('password', password);
+          alert('Sign Up Successful');
+          closePopup();
+      }
+  });
+
+
+  $('#loginForm').on('submit', function (e) {
+      e.preventDefault();
+      if ($(this).parsley().isValid()) {
+          var username = $('#loginUsername').val().trim();
+          var password = $('#loginPassword').val().trim();
+          var storedUsername = localStorage.getItem('username');
+          var storedPassword = localStorage.getItem('password');
+
+          if (username === storedUsername && password === storedPassword) {
+              alert('Login successful!');
+              closePopup();
+          } else {
+              $('#loginErrorMessage').text('Username or password is incorrect.');
+          }
+      }
+  });
+});
